0b0dc0c8fcf1d96fff2a84674ad7d9391d4081be
amir-wyvern <amirhosein_wyvern@yahoo.com>
2024-05-28 00:31:37 -0400

    feat: add proxy contract deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,6 +7,16 @@ async function deployDiamond () {
   const accounts = await ethers.getSigners()
   const contractOwner = accounts[0]
 
+  const backContractOBJ = await ethers.getContractFactory("BackContract");
+  const backContract = await backContractOBJ.deploy();
+  await backContract.deployed();
+  console.log("BackContract deployed to:", backContract.address);
+
+  const proxyContractOBJ = await ethers.getContractFactory("ProxyContract");
+  const proxyContract = await proxyContractOBJ.deploy(backContract.address);
+  await proxyContract.deployed();
+  console.log("proxyContract deployed to:", proxyContract.address);
+
   // deploy facets
   console.log('')
   console.log('Deploying facets')
