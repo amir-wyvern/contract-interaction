717b8c1d3838eb580186bfa20eb4915cc22492e5
amir-wyvern <amirhosein_wyvern@yahoo.com>
2024-06-02 19:32:12 -0400

    fix: add dev-comments and remove LibOwnership part

--- contracts/facets/DiamondCutFacet.sol
+++ contracts/facets/DiamondCutFacet.sol
@@ -1,31 +1,23 @@
-// SPDX-License-Identifier: BSD-3-Clause license
-pragma solidity 0.8.19;
-pragma experimental ABIEncoderV2;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-import "../interfaces/IDiamondCut.sol";
-import "../libraries/LibDiamond.sol";
-import "../libraries/LibOwnership.sol";
+import { IDiamondCut } from "../interfaces/IDiamondCut.sol";
+import { LibDiamond } from "../libraries/LibDiamond.sol";
+
 
 contract DiamondCutFacet is IDiamondCut {
+    /// @notice Add/replace/remove any number of functions and optionally execute
+    ///         a function with delegatecall
+    /// @param _diamondCut Contains the facet addresses and function selectors
+    /// @param _init The address of the contract or facet to execute _calldata
+    /// @param _calldata A function call, including function selector and arguments
+    ///                  _calldata is executed with delegatecall on _init
     function diamondCut(
         FacetCut[] calldata _diamondCut,
         address _init,
         bytes calldata _calldata
     ) external override {
-        LibOwnership.enforceIsContractOwner();
-
-        uint256 selectorCount = LibDiamondStorage.diamondStorage().selectors.length;
-        for (uint256 facetIndex; facetIndex < _diamondCut.length; facetIndex++) {
-            FacetCut memory cut;
-            cut.action = _diamondCut[facetIndex].action;
-            cut.facetAddress = _diamondCut[facetIndex].facetAddress;
-            cut.functionSelectors = _diamondCut[facetIndex].functionSelectors;
-
-            selectorCount = LibDiamond.executeDiamondCut(selectorCount, cut);
-        }
-
-        emit DiamondCut(_diamondCut, _init, _calldata);
-
-        LibDiamond.initializeDiamondCut(_init, _calldata);
+        LibDiamond.enforceIsContractOwner();
+        LibDiamond.diamondCut(_diamondCut, _init, _calldata);
     }
 }
