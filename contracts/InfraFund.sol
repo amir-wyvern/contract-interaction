2d7ba7f8cc0b9a93736b6f96dcd62dc42533d4f8
amir-wyvern <amirhosein_wyvern@yahoo.com>
2024-06-02 19:30:46 -0400

    fix: remove inital facet and use of LibDiamond instead of it

--- contracts/InfraFund.sol
+++ contracts/InfraFund.sol
@@ -16,41 +16,35 @@ $$$$$$/ $$/   $$/ $$/     $$/       $$$$$$$/       $$/      $$$$$$/  $$/   $$/  
 
 import { LibDiamond } from "./libraries/LibDiamond.sol";
 import { IDiamondCut } from "./interfaces/IDiamondCut.sol";
-import { LibDiamondStorage } from "./libraries/LibDiamondStorage.sol";
-import { LibOwnership } from "./libraries/LibOwnership.sol";
-import "./interfaces/IERC165.sol";
-import "./interfaces/IERC173.sol";
-import "./interfaces/IDiamondLoupe.sol";
-
 
 contract InfraFund {    
 
-    constructor(IDiamondCut.FacetCut[] memory _diamondCut, address _owner) payable {
-        require(_owner != address(0), "owner must not be 0x0");
+    constructor(address _contractOwner, address _diamondCutFacet) payable {        
+        LibDiamond.setContractOwner(_contractOwner);
 
-        LibDiamond.diamondCut(_diamondCut, address(0), new bytes(0));
-        LibOwnership.setContractOwner(_owner);
-
-        LibDiamondStorage.DiamondStorage storage ds = LibDiamondStorage.diamondStorage();
-        // adding ERC165 data
-        ds.supportedInterfaces[type(IERC165).interfaceId] = true;
-        ds.supportedInterfaces[type(IDiamondCut).interfaceId] = true;
-        ds.supportedInterfaces[type(IDiamondLoupe).interfaceId] = true;
-        ds.supportedInterfaces[type(IERC173).interfaceId] = true;       
+        // Add the diamondCut external function from the diamondCutFacet
+        IDiamondCut.FacetCut[] memory cut = new IDiamondCut.FacetCut[](1);
+        bytes4[] memory functionSelectors = new bytes4[](1);
+        functionSelectors[0] = IDiamondCut.diamondCut.selector;
+        cut[0] = IDiamondCut.FacetCut({
+            facetAddress: _diamondCutFacet, 
+            action: IDiamondCut.FacetCutAction.Add, 
+            functionSelectors: functionSelectors
+        });
+        LibDiamond.diamondCut(cut, address(0), "");        
     }
 
     // Find facet for function that is called and execute the
     // function if a facet is found and return any value.
     fallback() external payable {
-
-        LibDiamondStorage.DiamondStorage storage ds;
-
+        LibDiamond.DiamondStorage storage ds;
+        bytes32 position = LibDiamond.DIAMOND_STORAGE_POSITION;
+        assembly {
+            ds.slot := position
+        }
         // get facet from function selector
-        // address facet = ds.selectorToFacetAndPosition[msg.sig].facetAddress;
-        address facet = address(bytes20(ds.facets[msg.sig].facetAddress));
-
+        address facet = ds.selectorToFacetAndPosition[msg.sig].facetAddress;
         require(facet != address(0), "Diamond: Function does not exist");
-
         // Execute external function from facet using delegatecall and return any value.
         assembly {
             // copy function selector and any arguments
