0b7a6a45350007591c36b3372c34c15819b775b4
amir-wyvern <amirhosein_wyvern@yahoo.com>
2024-06-02 19:41:02 -0400

    fix: add more functionality

--- scripts/diamond_deploy.js
+++ scripts/diamond_deploy.js
@@ -7,14 +7,32 @@ async function deployDiamond () {
   const accounts = await ethers.getSigners()
   const contractOwner = accounts[0]
 
+  // deploy DiamondCutFacet
+  const DiamondCutFacet = await ethers.getContractFactory('DiamondCutFacet')
+  const diamondCutFacet = await DiamondCutFacet.deploy()
+  await diamondCutFacet.deployed()
+  console.log('DiamondCutFacet deployed:', diamondCutFacet.address)
+
+  // deploy Diamond
+  const Diamond = await ethers.getContractFactory('InfraFund')
+  const diamond = await Diamond.deploy(contractOwner.address, diamondCutFacet.address)
+  await diamond.deployed()
+  console.log('InfraFund Diamond deployed:', diamond.address)
+
+  // deploy DiamondInit
+  // DiamondInit provides a function that is called when the diamond is upgraded to initialize state variables
+  const DiamondInit = await ethers.getContractFactory('DiamondInit')
+  const diamondInit = await DiamondInit.deploy()
+  await diamondInit.deployed()
+  console.log('DiamondInit deployed:', diamondInit.address)
+
   // deploy facets
   console.log('')
   console.log('Deploying facets')
   const FacetNames = [
-    'PermissionManagement',
-    'DiamondCutFacet',
     'DiamondLoupeFacet',
-    'OwnershipFacet'
+    'OwnershipFacet',
+    'PermissionManagement'
   ]
   const cut = []
   for (const FacetName of FacetNames) {
@@ -31,11 +49,19 @@ async function deployDiamond () {
 
   // upgrade diamond with facets
   console.log('')
-  console.log('Diamond Cut:', cut)
-  const infraFund = await ethers.getContractFactory("InfraFund");
-  const infra_fund = await infraFund.deploy(cut, contractOwner.address);
-  await infra_fund.deployed();
-  console.log("InfraFund deployed to:", infra_fund.address);
+  const diamondCut = await ethers.getContractAt('IDiamondCut', diamond.address)
+  let tx
+  let receipt
+  // call to init function
+  let functionCall = diamondInit.interface.encodeFunctionData('init')
+  tx = await diamondCut.diamondCut(cut, diamondInit.address, functionCall)
+  console.log('Diamond cut tx: ', tx.hash)
+  receipt = await tx.wait()
+  if (!receipt.status) {
+    throw Error(`Diamond upgrade failed: ${tx.hash}`)
+  }
+  console.log('Completed diamond cut')
+  return diamond.address
 }
 
 if (require.main === module) {
