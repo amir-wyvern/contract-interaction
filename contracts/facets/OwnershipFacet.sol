8977393c9702ede4ae0b5cebcf263a3023cebd4d
amir-wyvern <amirhosein_wyvern@yahoo.com>
2024-06-02 19:34:56 -0400

    cleanup

--- contracts/facets/OwnershipFacet.sol
+++ contracts/facets/OwnershipFacet.sol
@@ -1,16 +1,16 @@
-// SPDX-License-Identifier: BSD-3-Clause license
-pragma solidity 0.8.19;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-import "../libraries/LibOwnership.sol";
-import "../interfaces/IERC173.sol";
+import { LibDiamond } from "../libraries/LibDiamond.sol";
+import { IERC173 } from "../interfaces/IERC173.sol";
 
 contract OwnershipFacet is IERC173 {
     function transferOwnership(address _newOwner) external override {
-        LibOwnership.enforceIsContractOwner();
-        LibOwnership.setContractOwner(_newOwner);
+        LibDiamond.enforceIsContractOwner();
+        LibDiamond.setContractOwner(_newOwner);
     }
 
     function owner() external override view returns (address owner_) {
-        owner_ = LibOwnership.contractOwner();
+        owner_ = LibDiamond.contractOwner();
     }
 }
